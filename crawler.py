d4dea4971de91206684aff73800e45a6f4205230
jason3e7 <jason3e7@gmail.com>
2015-10-13 16:27:58 +0800

    crawler_nsfocus

--- crawler.py
+++ crawler.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 # coding=utf-8
 
 import sys
@@ -11,6 +11,7 @@ import csv
 from BeautifulSoup import BeautifulSoup
 from requests import Request, Session
 from datetime import datetime
+import opencc
 
 vulsListFile = open('./vulsList.csv', 'wb')
 VLWriter = csv.writer(vulsListFile)
@@ -18,18 +19,18 @@ VLWriter = csv.writer(vulsListFile)
 data = [['Date', 'Title', 'Platform']]
 VLWriter.writerows(data)
 
+begin = datetime(2015, 9, 17)
+end = datetime(2015, 9, 24)
+
 #urlList = []
 
 urlList = [
 	'https://www.exploit-db.com/remote/',
 	'https://www.exploit-db.com/webapps/',
 	'https://www.exploit-db.com/local/',
-	'https://www.exploit-db.com/dos/',
+	'https://www.exploit-db.com/dos/'
 	]
 
-
-begin = datetime(2015, 9, 17)
-end = datetime(2015, 9, 24)
 
 for url in urlList:
 	VLWriter.writerows([[url]])
@@ -71,6 +72,33 @@ for row in rows:
 		data = [[date, cells[1].getText()]]
 		VLWriter.writerows(data)
 
+time.sleep(0.5)
+
+#url = 'http://www.nsfocus.net/index.php?act=sec_bug'
+url = 'http://www.nsfocus.net/index.php?act=sec_bug&page=6'
+VLWriter.writerows([[url]])
+s = Session()
+req = Request('GET', url)
+prepped = req.prepare()
+prepped.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36' 
+r = s.send(prepped)
+print 'Get : %s' % url
+
+r.encoding = r.apparent_encoding
+
+soup = BeautifulSoup(r.text)
+rows = soup.find("ul", attrs={"class": "vul_list"}).findAll("li")
+cc = opencc.OpenCC('s2t')
+
+for row in rows:
+	# cn word print ERROR but save file OK
+	date = datetime.strptime(row.find("span").getText(), "%Y-%m-%d")  
+	if (begin <= date and date <= end):
+		# save utf8 tw use excel import OK
+		title = cc.convert(row.find("a").getText())
+		data = [[date, title]]
+		VLWriter.writerows(data)
+	
 time.sleep(0.5)
 
 vulsListFile.close()
